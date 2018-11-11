7f7f6b50992fead4500052d7e1768a467e832a88
Sebbe <seb_vb@hotmail.com>
2018-11-11 23:55:25 +0100

    background, colours, trails

---
+++ Double Pendulum/Assets/Scripts/Pendulum/TrailValue.cs
@@ -0,0 +1,15 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class TrailValue : MonoBehaviour {
+
+    public void ToggleTrail()
+    {
+        TrailRenderer trail1 = GameObject.Find("Particle1").GetComponent<TrailRenderer>();
+        TrailRenderer trail2 = GameObject.Find("Particle2").GetComponent<TrailRenderer>();
+
+        trail1.enabled = !trail1.enabled;
+        trail2.enabled = !trail2.enabled;
+    }
+}
